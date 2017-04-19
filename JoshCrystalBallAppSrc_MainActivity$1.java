9779cc4146935639ced8c1061b8988173df1454e
Josh <JScott5088@users.noreply.github.com>
2017-04-19 13:33:33 -0400

    Create JoshCrystalBallAppSrc_MainActivity$1.java

---
+++ JoshCrystalBallAppSrc_MainActivity$1.java
@@ -0,0 +1,23 @@
+package com.example.crystalball;
+
+
+// Referenced classes of package com.example.crystalball:
+//            MainActivity
+
+class this._cls0
+    implements nShakeListener
+{
+
+    final MainActivity this$0;
+
+    public void onShake()
+    {
+        MainActivity.access$0(MainActivity.this);
+    }
+
+    nShakeListener()
+    {
+        this$0 = MainActivity.this;
+        super();
+    }
+}
