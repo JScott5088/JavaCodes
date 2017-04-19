96db5bfe5fcae5e21a3d45aa8a379a1bf7194d09
Josh <JScott5088@users.noreply.github.com>
2017-04-19 13:36:19 -0400

    Create JoshCrystalBallAppSrc_R$anim.java

---
+++ JoshCrystalBallAppSrc_R$anim.java
@@ -0,0 +1,20 @@
+package com.example.crystalball;
+
+
+// Referenced classes of package com.example.crystalball:
+//            R
+
+public static final class 
+{
+
+    public static final int abc_fade_in = 0x7f040000;
+    public static final int abc_fade_out = 0x7f040001;
+    public static final int abc_slide_in_bottom = 0x7f040002;
+    public static final int abc_slide_in_top = 0x7f040003;
+    public static final int abc_slide_out_bottom = 0x7f040004;
+    public static final int abc_slide_out_top = 0x7f040005;
+
+    public ()
+    {
+    }
+}
