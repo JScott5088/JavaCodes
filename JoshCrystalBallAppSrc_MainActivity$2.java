a548771bef884c4d4e8f4334f05033ec8407f966
Josh <JScott5088@users.noreply.github.com>
2017-04-19 13:34:31 -0400

    Create JoshCrystalBallAppSrc_MainActivity$2.java

---
+++ JoshCrystalBallAppSrc_MainActivity$2.java
@@ -0,0 +1,24 @@
+package com.example.crystalball;
+
+import android.media.MediaPlayer;
+
+// Referenced classes of package com.example.crystalball:
+//            MainActivity
+
+class this._cls0
+    implements android.media.ompletionListener
+{
+
+    final MainActivity this$0;
+
+    public void onCompletion(MediaPlayer mediaplayer)
+    {
+        mediaplayer.release();
+    }
+
+    istener()
+    {
+        this$0 = MainActivity.this;
+        super();
+    }
+}
